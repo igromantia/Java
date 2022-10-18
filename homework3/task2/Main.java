37c6838469fdf3f923cfb9db75d3944590c44345
Игорь Лукин <igromantia@yandex.ru>
2022-10-18 19:06:23 +0300

    Add files via upload

---
+++ homework3/task2/Main.java
@@ -0,0 +1,24 @@
+package homework3.task2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите длинны сторон треугольника");
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        if (a < b + c ){
+            if (b < a + c){
+                if (c < b + a){
+                    System.out.println("Треугольник существует");
+                }
+                else System.out.println("Треугольник не существует");
+            }
+            else System.out.println("Треугольник не существует");
+        }
+        else System.out.println("Треугольник не существует");
+
+    }
+}
